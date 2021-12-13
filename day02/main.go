250a203a48abd420be0c80aa3f6ac1d074677d6d
Jiří Hejduk <jiri@hejduk.in>
2021-12-13 09:53:33 +0000

    day 2 completed

--- day02/main.go
+++ day02/main.go
@@ -11,6 +11,8 @@ func main() {
 	f.Println("Hello from Advent of Code - Day 2")
 	f.Println("-- First Part --")
 	FirstPart()
+	f.Println("-- Second Part --")
+	SecondPart()
 }
 
 func FirstPart() {
@@ -39,6 +41,40 @@ func FirstPart() {
 			vPos -= value
 		case "down":
 			vPos += value
+		}
+	}
+
+	f.Printf("H position: %d; V position: %d\n", hPos, vPos)
+	f.Printf("My answear: %d\n", (hPos * vPos))
+}
+
+func SecondPart() {
+	bytes, err := os.ReadFile("./input.txt")
+	if err != nil {
+		f.Printf("error during reading file :: %s\n", err)
+		os.Exit(1)
+	}
+
+	data := strings.Split(string(bytes), "\n")
+	f.Printf("Number of records: %d\n", len(data))
+	hPos, vPos, aim := 0, 0, 0
+	for i, l := 0, len(data); i < l; i++ {
+		tokens := strings.Split(data[i], " ")
+		instruction := tokens[0]
+		value, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			f.Printf("error during conversion of string to int :: %s\n", err)
+			os.Exit(1)
+		}
+
+		switch instruction {
+		case "forward":
+			hPos += value
+			vPos += aim * value
+		case "up":
+			aim -= value
+		case "down":
+			aim += value
 		}
 	}
 
